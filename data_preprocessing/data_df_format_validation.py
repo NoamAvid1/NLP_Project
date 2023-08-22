e20448e8d3da5cfa763b5f4f6d98e79ef580a901
t-noamavidor <t-noamavidor@microsoft.com>
2023-08-22 18:47:06 +0300

    Validated preprocessed data format

--- data_preprocessing/data_df_format_validation.py
+++ data_preprocessing/data_df_format_validation.py
@@ -18,11 +18,15 @@ def validate_original_sentence_from_model_question(row: pd.Series):
 
 
 def validate_model_question_suffix(row: pd.Series):
+    """
+    This function validates that each input for the model ends with " "
+    and that the inputs are formatted correctly according to the number of masks.
+    """
     model_question = row["model_question"]
     if type(model_question) != str:
         return model_question
     if not row['model_question'][-1] == " ":
-        # print(f"fixing row: {model_question}")
+        print(f"fixing row: {model_question}")
         model_question += " "
     if row['num_of_masks'] == 1 and not model_question.endswith("the "):
         print(f"\n\nerror in row {row[0]}:\nnum_of_masks={row['num_of_masks']}\n model question: {model_question}\nFix manually")
@@ -33,9 +37,9 @@ def validate_model_question_suffix(row: pd.Series):
 def main():
     df_path = "../preprocessed_data/experiment_sentences_preprocessed - All merged.csv"
     df = pd.read_csv(df_path)
-    df['model_question'] = df.apply(lambda row: validate_original_sentence_from_model_question(row), axis=1)
-    df['model_question'] = df.apply(lambda row: validate_model_question_suffix(row), axis=1)
-    # df.to_csv("../preprocessed_data/experiment_sentences_preprocessed - Noam 19.8.csv")
+    df['model_question'] = df.apply(lambda row: validate_original_sentence_from_model_question(row), axis=1, result_type='expand')
+    df['model_question'] = df.apply(lambda row: validate_model_question_suffix(row), axis=1, result_type='expand')
+    df.to_csv("../preprocessed_data/experiment_sentences_preprocessed - All merged.csv")
 
 if __name__ == '__main__':
     main()
