348e683d25cb79f1eca74042347e54ec2cf65a1a
offirdassa <offirdassa@mail.tau.ac.il>
2023-09-09 14:30:59 +0300

    analysis code

--- apply_avg_on_results.py
+++ apply_avg_on_results.py
@@ -2,26 +2,45 @@ import pandas as pd
 import argparse
 import os
 
-def apply_average(df,cols):
-    # df[col for col in cols] = df.apply(lambda row: func(row), axis=1, result_type='expand')
+
+def get_average(df,cols):
     false_avg = df[cols[0]].mean()
     correct_mask_1_avg = df[cols[1]].mean()
-    df.loc[len(df),cols[0]] = false_avg
-    df.loc[len(df)-1,cols[1]] = correct_mask_1_avg
-    if len(cols) > 2:
-        false_avg_mask_2_avg = df[cols[0][:-1]][df[cols[2]].notna()].mean()
+    if cols[2] in df.columns:
+        false_avg_mask_2_avg = df[cols[0]][df[cols[2]].notna()].mean()
         correct_mask_2_avg = df[cols[2]][df[cols[2]].notna()].mean()
-        df.loc[len(df),cols[0]] = false_avg_mask_2_avg
-        df.loc[len(df)-1,cols[2]] = correct_mask_2_avg
-    return df
+    else:
+        false_avg_mask_2_avg = pd.NA
+        correct_mask_2_avg = pd.NA
+    return {'false_mask1_avg': false_avg, 'correct_mask1_avg': correct_mask_1_avg, \
+            'false_mask2_avg': false_avg_mask_2_avg, 'correct_mask2_avg':correct_mask_2_avg }
+
+def get_accuracy(df):
+    df['is_greater_mask_1'] = df['correct_mask_1_score'] > df['false_answer_score']
+    accuracy_mask_1 = df['is_greater_mask_1'].mean()
+    accuracy_mask_2 = pd.NA
+    if 'correct_mask_2_score' in df.columns:
+         df['is_greater_mask_2'] =  df['correct_mask_2_score'] > df['false_answer_score']
+         accuracy_mask_2 = df['is_greater_mask_2'][df['correct_mask_2_score'].notna()].mean()
+    return {'accuracy_mask1': accuracy_mask_1, 'accuracy_mask2': accuracy_mask_2}
 
 
 if __name__ == '__main__':
-    dir = 'experiment_results'
+    dir = r'run_results\all_run'
     files = os.listdir(dir)
-    for file in files:
+    new_df = pd.DataFrame(columns=['model_name', 'model_step','false_mask1_avg', 'correct_mask1_avg', 'false_mask2_avg', 'correct_mask2_avg', 'accuracy_mask1', 'accuracy_mask2'])
+    for file in files: 
+        if file.startswith('google'):
+            split_arg = 2
+        else:
+            split_arg = 1
+        model_values =  {'model_name': '_'.join(file.split('_')[:-split_arg]),'model_step': '_'.join(file.split('_')[-split_arg:])[:-4]}
         file_path = os.path.join(dir,file)
-        if os.path.isfile(file_path):
-            df = pd.read_csv(file_path)
-            new_df = apply_average(df,['false_answer_score','correct_answer_score'])
-            new_df.to_csv(file_path, index=False)
+        df = pd.read_csv(file_path)
+        average_values = get_average(df,['false_answer_score','correct_mask_1_score','correct_mask_2_score'])
+        accuracy_values = get_accuracy(df)
+        model_values.update(average_values)
+        model_values.update(accuracy_values)
+
+        new_df = pd.concat([new_df,pd.DataFrame([model_values])],ignore_index=True)
+    new_df.to_csv('results_analysis/all_analysis.csv', index=False)
